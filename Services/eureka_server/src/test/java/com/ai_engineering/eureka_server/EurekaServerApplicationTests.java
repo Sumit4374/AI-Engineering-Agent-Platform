2db6c539f631e27872724e75f176a6006499c415
Sumit4374 <kusumit437@gmail.com>
2026-05-27 02:58:32 +0530

    Feat: eureka server and api gateway service initiation

---
+++ Services/eureka_server/src/test/java/com/ai_engineering/eureka_server/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai_engineering.eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
