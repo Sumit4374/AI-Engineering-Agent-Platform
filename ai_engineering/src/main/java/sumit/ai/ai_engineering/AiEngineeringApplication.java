1d02086fa3543ab5c2597890372496d7bf88cadd
Sumit4374 <kusumit437@gmail.com>
2026-08-25 15:52:20 +0530

    feat: Switching to Modular Monolith

---
+++ ai_engineering/src/main/java/sumit/ai/ai_engineering/AiEngineeringApplication.java
@@ -0,0 +1,13 @@
+package sumit.ai.ai_engineering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiEngineeringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiEngineeringApplication.class, args);
+	}
+
+}
